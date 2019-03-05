7354a2e38cea04e8514990e2a038e65f53d656db
digdef <bydigdef@gmail.com>
2019-03-05 20:21:55 +0200

    main page

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Store</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<div class="content">
+			<a href="">
+				<img class="logo" src="img/logo.png">				
+			</a>
+		</div>
+	</header>
+	<nav>
+		<button class="nav-btn">Категории</button>
+		<button class="nav-btn">Поиск</button>
+		<button class="nav-btn">Корзина</button>
+	</nav>
+
+	<div style="padding-top: 60px">
+		<div class="container">
+			<div class="container-fluid" id="content">
+				<div class="row text-center">
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/1.jpg" class="w-100 img">
+							<h3>Порошок</h3>
+							<h4>10$</h4>
+						</a>
+					</div>
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/2.jpg" class="w-100 img">
+							<h3>Трава</h3>
+							<h4>1$</h4>
+						</a>
+					</div>
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/3.jpg" class="w-100 img">
+							<h3>Верёвка</h3>
+							<h4>100$</h4>
+						</a>
+					</div>
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/4.jpg" class="w-100 img">
+							<h3>Мыло</h3>
+							<h4>120$</h4>
+						</a>
+					</div>
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/5.jpg" class="w-100 img">
+							<h3>Табурет</h3>
+							<h4>109$</h4>
+						</a>
+					</div>
+					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+						<a href="\" id="link">
+							<img src="img/6.jpg" class="w-100 img">
+							<h3>Снег</h3>
+							<h4>0$</h4>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<a id="link2" href="">telegram</a>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
