6b4eedea5c60a7315469640c8543664207992154
digdef <bydigdef@gmail.com>
2019-03-06 16:29:10 +0200

    navbar

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 </head>
 <body>
 	<header>
@@ -15,54 +16,66 @@
 		</div>
 	</header>
 	<nav class="nav">
+		<div id="genre-btn" href="#" onclick="toggle('#vipad');">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
 		<button class="nav-btn">Категории</button>
 		<button class="nav-btn">Корзина</button>
-		<form>
-			<input class="search" type="search" name="search" placeholder="Поиск">
-			<input class="submit" type="submit" name="submit" value="Поиск">
-		</form>	
+		<form class="search">
+			<input class="search-txt" type="search" name="search" placeholder="Поиск">
+			<button class="submit" type="submit" name="submit" value="Поиск">
+				<i class="fas fa-search"></i>
+			</button>
+		</form>
 	</nav>
-
+	<div id="vipad">
+		<div id="genre-bar">
+			<button class="vipad-btn">Категории</button><br>
+			<button class="vipad-btn">Корзина</button>
+		</div>
+	</div>
 	<div style="padding-top: 60px">
 		<div class="container">
 			<div class="container-fluid" id="content">
 				<div class="row text-center">
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/1.jpg" class="w-100 img">
 							<h3>Порошок</h3>
 							<h4>10$</h4>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/2.jpg" class="w-100 img">
 							<h3>Трава</h3>
 							<h4>1$</h4>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/3.jpg" class="w-100 img">
 							<h3>Верёвка</h3>
 							<h4>100$</h4>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/4.jpg" class="w-100 img">
 							<h3>Мыло</h3>
 							<h4>120$</h4>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/5.jpg" class="w-100 img">
 							<h3>Табурет</h3>
 							<h4>109$</h4>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-4 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
 						<a href="\" id="link">
 							<img src="img/6.jpg" class="w-100 img">
 							<h3>Снег</h3>
@@ -76,6 +89,7 @@
 	<footer>
 		<a id="link2" href="">telegram</a>
 	</footer>
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="js/main.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
