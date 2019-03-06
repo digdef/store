3456fb00448e7c97d8114540cea69b4d0d0fb265
digdef <bydigdef@gmail.com>
2019-03-06 22:50:16 +0200

    main page

--- index.php
+++ index.php
@@ -40,46 +40,82 @@
 		<div class="container">
 			<div class="container-fluid" id="content">
 				<div class="row text-center">
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/1.jpg" class="w-100 img">
-							<h3>Порошок</h3>
-							<h4>10$</h4>
+							<div class="name">
+								<div class="price">
+									1451$
+								</div>
+								<button class="ell">
+									Порошок
+								</button>
+							</div>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/2.jpg" class="w-100 img">
-							<h3>Трава</h3>
-							<h4>1$</h4>
+							<div class="name">
+								<div class="price">
+									1$
+								</div>
+								<button class="ell">
+									Трава
+								</button>
+							</div>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/3.jpg" class="w-100 img">
-							<h3>Верёвка</h3>
-							<h4>100$</h4>
+							<div class="name">
+								<div class="price">
+									100$
+								</div>
+								<button class="ell">
+									Верёвка
+								</button>
+							</div>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/4.jpg" class="w-100 img">
-							<h3>Мыло</h3>
-							<h4>120$</h4>
+							<div class="name">
+								<div class="price">
+									120$
+								</div>
+								<button class="ell">
+									Мыло
+								</button>
+							</div>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/5.jpg" class="w-100 img">
-							<h3>Табурет</h3>
-							<h4>109$</h4>
+							<div class="name">
+								<div class="price">
+									111$
+								</div>
+								<button class="ell">
+									Табурет
+								</button>
+							</div>
 						</a>
 					</div>
-					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img">
+					<div class="col-xs-2 col-sm-6 col-xl-4 tile-img shop">
 						<a href="\" id="link">
 							<img src="img/6.jpg" class="w-100 img">
-							<h3>Снег</h3>
-							<h4>0$</h4>
+							<div class="name">
+								<div class="price">
+									0$
+								</div>
+								<button class="ell">
+									Снег
+								</button>
+							</div>
 						</a>
 					</div>
 				</div>
@@ -89,7 +125,6 @@
 	<footer>
 		<a id="link2" href="">telegram</a>
 	</footer>
-
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="js/main.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
